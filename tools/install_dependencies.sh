5de316ebcd117e3b8489eb4fa2f0c459f620e1b3
Mees van Löben Sels <50023316+mvanlobensels@users.noreply.github.com>
2026-07-20 17:12:33 +0200

    PXA-580 SITL containers for arm (#1)
    * build docker for arm
    
    * Input parameters for axiom
    
    * remove speedup as launch argument, read as environment variable
    
    * Set root folder as variable
    
    * Update submodule branch

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -5,25 +5,28 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 
 # Set current working directory to the ros_ws directory
-cd "$(dirname "$0")"/../../..
+ROOT="$(cd "$(dirname "$0")"/../../.. && pwd)"
 
 # Install whatever rosdep can cover
 apt-get update
-rosdep update
-rosdep install -i --from-path . --rosdistro ${ROS_DISTRO} -y
+rosdep update --include-eol-distros
+rosdep install -i --from-path "$ROOT" --rosdistro ${ROS_DISTRO} -y
 
 # Install system dependencies not covered by rosdep
 apt-get install -y libignition-common3-graphics
-## Add 3rd party repositories
-apt-get install software-properties-common -y
-### iir1
-add-apt-repository ppa:berndporr/dsp -y
 ## Install packages
-apt-get update && apt-get install -y\
-    iir1-dev \
+apt-get install -y \
     python3-pip \
-    vim
+    vim \
+    cmake \
+    git
+
+# Install iir1 from source (PPA only has amd64 packages)
+git clone https://github.com/berndporr/iir1.git /tmp/iir1
+cd /tmp/iir1 && cmake -S . -B build && cmake --build build && cmake --install build
+ldconfig
 
 # Install pip packages
+pip install "cython<3"
 pip install /ros_ws/src/last_letter/last_letter/external/last_letter_lib/
-pip3 install -r src/last_letter/tools/requirements.txt
+pip3 install -r "$ROOT"/src/last_letter/tools/requirements.txt
